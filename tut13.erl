749d6e0d1b0be1b2e0f9b618206329da7a9cbf6a
Frode Aannevik <frode.aa@gmail.com>
2015-01-25 22:03:00 +0100

    tut13:convert_list_to_c/1
          tut13:convert_list_to_c([
    			{moscow, {c, -10}}, {cape_town, {f, 70}},
    			{stockholm, {c, -4}}, {paris, {f, 28}}, 			{london, {f, 36}}]).

--- tut13.erl
+++ tut13.erl
@@ -8,5 +8,7 @@ convert_to_c({Name, {c, Temp}}) ->
 				{Name, {c, Temp}}.
 
 convert_list_to_c(List) ->
-				lists:map(fun convert_to_c/1, List).
+				New_list = lists:map(fun convert_to_c/1, List),
+				lists:sort(fun({_, {c, Temp1}}, {_, {c, Temp2}}) ->
+																	 Temp1 < Temp2 end, New_list).
 
